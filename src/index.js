8fa8e57583338f3c75d755924c6b42b5b9601ae4
Connor <connordbrown@gmail.com>
2023-08-29 15:05:07 -0600

    performs Digimon filter function correctly

--- src/index.js
+++ src/index.js
@@ -6,21 +6,43 @@ document.addEventListener('DOMContentLoaded', () => {
 function fetchDigimon() {
     fetch("http://localhost:3000/digimon")
       .then(response => response.json())
-      .then(data => fillPage(data))
+      .then(data => {
+            fillPage(data);
+            filterDigimon(data);
+      })
 }
 
 
 function fillPage(digiData) {
+    const digimonContainer = document.querySelector('#digimon-container');
+        digimonContainer.textContent = "";
+
     const digiList = document.createElement('ul');
+        digiList.id = 'digimon-list';
+
     digiData.forEach(digimon => {
         const {name} = digimon;
         const digiName = document.createElement('li');
-            digiName.textContent = name;
+            digiName.textContent = `${name} `;
         const digiButton = document.createElement('button');
             digiButton.textContent = 'view';
         digiName.appendChild(digiButton);
+
         digiList.appendChild(digiName);
     })
-    const digimonContainer = document.querySelector('#digimon-container');
+
     digimonContainer.appendChild(digiList);
 }
+
+
+function filterDigimon(digiData) {
+    const selectLevel = document.querySelector('#level-dropdown');
+    selectLevel.addEventListener('change', () => {
+        let digiFiltered = digiData.filter((digimon) => {
+            return digimon.level === selectLevel.value;
+        })
+        fillPage(digiFiltered);  
+    }) 
+
+}
+
